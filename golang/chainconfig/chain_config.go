b4a3c1ac27e57b3a8a9046d0c2d6680c50b1a3e5
0xNotMe <nme15500@gmail.com>
2024-10-11 17:14:00 +1100

    fix: add symlink for config

--- golang/chainconfig/chain_config.go
+++ golang/chainconfig/chain_config.go
@@ -53,7 +53,7 @@ var chainConfig map[string]ChainConfig
 
 func init() {
 	// Load the config file
-	configPath := "../../configs/chain-config.json"
+	configPath := "../configs/chain-config.json"
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
 		configPath = "./chain-config.json"
 	}
